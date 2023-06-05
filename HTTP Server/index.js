3d462c37af68fbf103fd39a9b988acce1e024e00
Mariga <mariga.bn@gmail.com>
2023-06-05 23:17:59 +0300

    Update

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -5,5 +5,5 @@ const logger = new Logger();
 logger.on("message", (data) => console.log("Called Listener: ", data));
 
 logger.log("Mariga Bruce!");
-logger.log("Brenin Nyaribo !");
-logger.log("Brenda Moraa !");
+logger.log("Brenin Nyaribo!");
+logger.log("Brenda Moraa!");

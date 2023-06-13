7a7126924852673f09162d7829630700bc13b837
Mariga Bruce <mariga.bn@gmail.com>
2023-06-13 09:42:57 +0300

    Update

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
           "Content-Type": "text/html",
         });
-        res.end("<h1>Home</h1>");
+        res.end("<h1>Hiii</h1>");
       }
     );
   }
